967e11081f570aae0477039068b5ddd39917beb4
Loren Siebert <loren@siebert.org>
2015-05-04 09:35:07 -0700

    OK to just write the application name in there.

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-APP_NAME = Rails.application.config.session_options[:key].sub(/^_/,'').sub(/_session/,'')
+APP_NAME = 'i14y'
