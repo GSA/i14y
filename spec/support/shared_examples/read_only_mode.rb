732a9ce86ba46f66c332a98f392809cdab99fdff
Nathan Smith <noremmie@gmail.com>
2017-12-14 12:18:58 -0800

    squashme: add newlines for readability

--- spec/support/shared_examples/read_only_mode.rb
+++ spec/support/shared_examples/read_only_mode.rb
@@ -3,6 +3,9 @@ shared_examples 'a data modifying request made during read-only mode' do
 
   it 'returns a read-only-mode-releated failure message as JSON' do
     expect(response.status).to eq(503)
-    expect(JSON.parse(response.body)).to match(hash_including('status' => 503, 'developer_message' => 'The i14y API is currently in read-only mode.'))
+    expect(JSON.parse(response.body)).to match(hash_including({
+      'status' => 503,
+      'developer_message' => 'The i14y API is currently in read-only mode.'
+    }))
   end
 end
