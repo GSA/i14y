899f5e25eae77cff12ad9317bb3a14a2e49ff7bb
Martha Thompson <437455+MothOnMars@users.noreply.github.com>
2020-01-16 13:39:53 -0800

    SRCH-1230 use text type for bigrams (#107)

--- app/templates/documents.rb
+++ app/templates/documents.rb
@@ -186,7 +186,7 @@ class Documents
   def bigrams(json)
     json.bigrams do
       json.analyzer "bigrams_analyzer"
-      json.type "string"
+      json.type "text"
     end
   end
 
@@ -253,7 +253,7 @@ class Documents
       json.child! do
         json.set! locale do
           json.match "*_#{locale}"
-          json.match_mapping_type "string"
+          json.match_mapping_type "text"
           json.mapping do
             json.analyzer "#{locale}_analyzer"
             json.type "text"
