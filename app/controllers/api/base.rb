9c46ea823b7c89e7158e1fb22ce364e029a8c3aa
John Maxwell <jmaxwell@fearless.tech>
2021-06-04 14:13:53 -0400

    Removed airbrake, its configuration files, and a bit of code.
    Also added rake to the Gemfile.

--- app/controllers/api/base.rb
+++ app/controllers/api/base.rb
@@ -17,8 +17,6 @@ module API
     rescue_from :all do |e|
       Rails.logger.error "#{e}\n\n#{e.backtrace.join("\n")}"
 
-      Airbrake.notify(e)
-
       rack_response({ developer_message: "Something unexpected happened and we've been alerted.", status: 500 }.to_json, 500)
     end
 
