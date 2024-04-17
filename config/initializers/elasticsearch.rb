b2bf9c7ef12a32dfbc44bd518402473a6d46989f
sb-ebukaanene <ebuka.anene@semanticbits.com>
2024-04-17 12:56:25 -0700

    added circle ci

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -4,7 +4,7 @@ module ES
   ES_CONFIG = Rails.application.config_for(:elasticsearch).freeze
 
   def self.client
-    Elasticsearch::Client.new(ES_CONFIG.merge({randomize_hosts: true, retry_on_failure: true, reload_connections: false, reload_on_failure: false}))
+    Elasticsearch::Client.new(ES_CONFIG.merge({randomize_hosts: true, retry_on_failure: true, reload_connections: false, reload_on_failure: false, reload_on_failure: false, transport_options: { ssl: { verify: ENV.fetch("SSL_VERIFY") }}}))
   end
 
   def self.collection_repository
