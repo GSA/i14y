274c637d8a1742d3be716eacee6f44fd1451396b
Steven Barragán Naranjo <850686+stevenbarragan@users.noreply.github.com>
2024-10-21 19:23:29 -0700

    Fix config hosts (#343)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.rails_semantic_logger.format = :json
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts += ENV['I14Y_ALLOWED_HOSTS'] if ENV['I14Y_ALLOWED_HOSTS'].present?
+  config.hosts << ENV['I14Y_ALLOWED_HOSTS'] if ENV['I14Y_ALLOWED_HOSTS'].present?
   
   # Skip DNS rebinding protection for the default health check endpoint.
   config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
