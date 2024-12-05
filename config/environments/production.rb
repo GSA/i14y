0a8cca0b5f4c07c203146d04cd0c8ff36f846828
Bhavith Katpally <krbhavith@gmail.com>
2024-12-05 12:30:15 -0500

    Sync with staging (#353)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts << ENV['I14Y_ALLOWED_HOSTS'] if ENV['I14Y_ALLOWED_HOSTS'].present?
-
+  
   # Skip DNS rebinding protection for the default health check endpoint.
   config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
